ee23c3d4ddbc1f4d4d8fddc9f2f4b9eb69901e5c
Mohamed Abo ElNaga <mohamedaboelnaga@Mohameds-MacBook-Air-2.local>
2025-08-19 12:59:50 +0300

    feat: Integrate custom theme preset for PrimeVue components
    - Add a custom theme preset using definePreset for enhanced semantic color schemes in both light and dark modes.
    - Update PrimeVue configuration to utilize the new preset, improving visual consistency across the application.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import App from './App.vue'
 // PrimeVue Theme Setup
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
+import { definePreset } from '@primeuix/themes';
 import ConfirmationService from 'primevue/confirmationservice';
 
 import '@/assets/styles.scss';
@@ -13,12 +14,50 @@ import '@/assets/styles.scss';
 
 const pinia = createPinia()
 const app = createApp(App)
+const MyPreset = definePreset(Aura, {
+  semantic: {
+      colorScheme: {
+          light: {
+              surface: {
+                  0: '#ffffff',
+                  50: '{slate.50}',
+                  100: '{slate.100}',
+                  200: '{slate.200}',
+                  300: '{slate.300}',
+                  400: '{slate.400}',
+                  500: '{slate.500}',
+                  600: '{slate.600}',
+                  700: '{slate.700}',
+                  800: '{slate.800}',
+                  900: '{slate.900}',
+                  950: '{slate.950}'
+              }
+          },
+          dark: {
+              surface: {
+                  0: '#ffffff',
+                  50: '{slate.50}',
+                  100: '{slate.100}',
+                  200: '{slate.200}',
+                  300: '{slate.300}',
+                  400: '{slate.400}',
+                  500: '{slate.500}',
+                  600: '{slate.600}',
+                  700: '{slate.700}',
+                  800: '{slate.800}',
+                  900: '{slate.900}',
+                  950: '{slate.950}'
+              }
+          }
+      }
+  }
+});
 
 app.use(pinia)
 app.use(router)
 app.use(PrimeVue, {
   theme: {
-      preset: Aura,
+      preset: MyPreset,
       options: {
           darkModeSelector: '.app-dark'
       }
